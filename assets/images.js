0e66bd0fda42f8bdd061e4e102dc3c8ba764afb3
RightShiner <brilight.shiner@gmail.com>
2023-06-14 15:15:08 +0200

    init project

---
+++ assets/images.js
@@ -0,0 +1,11 @@
+import clearSky from "../assets/clear-sky.png"
+import fewClouds from "../assets/few-clouds.png"
+import scatteredClouds from "../assets/scattered-clouds.png"
+import overcast from "../assets/overcast.png"
+import fog from "../assets/fog.png"
+import rain from "../assets/rain.png"
+import showerRain from "../assets/shower-rain.png"
+import snow from "../assets/snow.png"
+import thunderstorm from "../assets/thunderstorm.png"
+
+export {clearSky, fewClouds, scatteredClouds, overcast, fog, rain, showerRain, snow, thunderstorm}
